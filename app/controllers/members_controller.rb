ce5f9e352a8e670218bdd0022b534045908dc499
Jaque Elizandro <jaque@elizandro.com.br>
2022-05-25 18:55:22 +1000

    create index members

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,11 @@
 class MembersController < ApplicationController
 
+    def index
+        group = Group.find(params[:group_id])
+        members = group.members
+        render json: members
+    end
+    
     def create
         member = Member.create(name: params[:name], email: params[:email])
         group = Group.find(params[:group_id])
