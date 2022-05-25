93f9fcb650571c2c1871d7c5a8dccce187274840
Jaque Elizandro <jaque@elizandro.com.br>
2022-05-25 18:56:25 +1000

    set up the associations and routes

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,4 @@
 class Member < ApplicationRecord
     belongs_to :group
+    has_many :transactions
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :groups, :only => [:create, :show] do
-    resources :members, :only => [:create]
+  resources :groups, only: [:create, :show] do
+    resources :members, only: [:create, :index]
+    post '/inicialmembers', to: 'members#create'
+    resources :transactions, only: [:create, :index]
+    resources :debts, only: [:index]
   end
 end
