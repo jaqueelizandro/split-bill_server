d7899c0a51494372ff671a472f2401c1cb99b53c
Jaque Elizandro <jaque@elizandro.com.br>
2022-05-28 20:06:08 +1000

    create transaction settle and income

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -7,6 +7,38 @@ class TransactionsController < ApplicationController
     end
 
     def create
+        if params[:kind] == 'settle'
+            ActiveRecord::Base.transaction do
+                trans_settle = Transaction.create!(
+                    kind: 'settle',
+                    description: params[:description],
+                    amount: params[:amount],
+                    date: params[:date],
+                    image: params[:image],
+                    group_id: params[:group_id],
+                    member_id: params[:who_paid]
+                )
+                group = Group.find(params[:group_id])
+                group.transactions << trans_settle
+
+                trans_income = Transaction.create!(
+                    kind: 'income',
+                    description: params[:description],
+                    amount: params[:amount],
+                    date: params[:date],
+                    image: params[:image],
+                    group_id: params[:group_id],
+                    member_id: params[:paid_for]
+                )
+                group = Group.find(params[:group_id])
+                group.transactions << trans_income
+            end
+            # render json: [trans_expense, trans_income], status: :created
+            # respond_to do |format|
+            #     format.json  { render :json => {:trans_expense => trans_expense, 
+            #                                     :trans_income => trans_income }}
+            #   end
+        else
         transaction = Transaction.create!(
             kind: params[:kind],
             description: params[:description],
@@ -19,6 +51,8 @@ class TransactionsController < ApplicationController
         group = Group.find(params[:group_id])
         group.transactions << transaction
         transactions = group.transactions
+
         render json: transactions, status: :created
+        end
     end
 end
