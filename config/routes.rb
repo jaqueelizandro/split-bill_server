ea18f532e9471eabebefbe45a4ef4d74b75e5f56
Jaque Elizandro <jaque@elizandro.com.br>
2022-05-30 14:39:16 +1000

    add routes update and destroy to transactions and members

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :groups, only: [:create, :show] do
-    resources :members, only: [:create, :index]
+    resources :members, only: [:create, :index, :update, :destroy]
     post '/inicialmembers', to: 'members#create'
-    resources :transactions, only: [:create, :index]
+    resources :transactions, only: [:create, :index, :update, :destroy]
     resources :debts, only: [:index]
     resources :settle, only: [:create]
   end
