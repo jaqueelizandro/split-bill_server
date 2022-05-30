8e7a0f1482e8bfb111ea632b50cbb541b88719a0
Jaque Elizandro <jaque@elizandro.com.br>
2022-05-30 14:37:42 +1000

    update and destroy members

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -7,10 +7,24 @@ class MembersController < ApplicationController
     end
     
     def create
-        member = Member.create(name: params[:name], email: params[:email])
+        member = Member.create!(name: params[:name], email: params[:email])
         group = Group.find(params[:group_id])
         group.members << member
         members = group.members
         render json: members, status: :created
+    end
+
+    def update
+        group = Group.find(params[:group_id])
+        member = group.members.find(params[:id])
+        member.update!(name: params[:name], email: params[:email])
+        render json: member, status: :created
+    end
+
+    def destroy
+        group = Group.find(params[:group_id])
+        member = group.members.find(params[:id])
+        member.destroy!
+        render json: member, status: :no_content
     end
 end
